535df5870b6fdbed967f55188a96f0c93a536eaa
EnergyPatrikHU <energypatrikhu@proton.me>
2024-07-25 19:18:15 +0200

    Handle negative numbers in numPrefix function

--- src/utils/numPrefix.ts
+++ src/utils/numPrefix.ts
@@ -6,5 +6,6 @@
  * @returns A string representation of the number with leading zeros.
  */
 export function numPrefix(num: number, placeholders: number = 0): string {
-	return '0'.repeat(placeholders - (placeholders > 1 ? num.toString().length : 1)) + num;
+  if (num < 0) num = 0;
+  return '0'.repeat(placeholders - (placeholders > 1 ? num.toString().length : 1)) + num;
 }
